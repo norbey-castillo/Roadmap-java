1f62929a21f8b268af02a39eb02ebf5f6a0e6fe3
Norbey <norbeycastillo890@gmail.com>
2026-06-02 13:49:14 -0600

    type Casting, Comverting String To Int

---
+++ src/typeCasting/ComvertingStringToInt.java
@@ -0,0 +1,15 @@
+package typeCasting;
+
+public class ComvertingStringToInt {
+		  public static void main(String[] args) {
+		    // create string type variable
+		    String data = "10";
+		    System.out.println("The string value is: " + data);
+
+		    // convert string variable to int
+		    int num = Integer.parseInt(data);
+		    System.out.println("The integer value is: " + num);
+		  
+		}
+
+}
