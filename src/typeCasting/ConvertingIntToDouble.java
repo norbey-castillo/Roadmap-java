4fcd44f1d9a42892e1fe7c2377c7f1cf5bd55320
Norbey <norbeycastillo890@gmail.com>
2026-06-02 13:38:26 -0600

    Type Casting, Comverting Int To Double

--- src/typeCasting/ConvertingIntToDouble.java
+++ src/typeCasting/ConvertingIntToDouble.java
@@ -3,7 +3,13 @@ package typeCasting;
 public class ConvertingIntToDouble {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// create int type variable
+	    int num = 10;
+	    System.out.println("The integer value: " + num);
+
+	    // convert into double type
+	    double data = num;
+	    System.out.println("The double value: " + data);
 
 	}
 
