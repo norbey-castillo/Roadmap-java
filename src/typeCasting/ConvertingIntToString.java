bab32d656c03b6824c828532a82c595aef92fb0f
Norbey <norbeycastillo890@gmail.com>
2026-06-02 13:46:31 -0600

    Type Casting, Comverting Int To String

---
+++ src/typeCasting/ConvertingIntToString.java
@@ -0,0 +1,15 @@
+package typeCasting;
+
+public class ConvertingIntToString {
+		  public static void main(String[] args) {
+		    // create int type variable
+		    int num = 10;
+		    System.out.println("The integer value is: " + num);
+
+		    // converts int to string type
+		    String data = String.valueOf(num);
+		    System.out.println("The string value is: " + data);
+		  
+		}
+
+}
