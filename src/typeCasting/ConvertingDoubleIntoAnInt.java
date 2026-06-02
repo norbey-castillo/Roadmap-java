92736ea54dcbd81a61bf0c2b367035c1d10077fc
Norbey <norbeycastillo890@gmail.com>
2026-06-02 13:43:07 -0600

    Type Casting, Converting Double Into An Int

---
+++ src/typeCasting/ConvertingDoubleIntoAnInt.java
@@ -0,0 +1,16 @@
+package typeCasting;
+
+public class ConvertingDoubleIntoAnInt {
+
+	public static void main(String[] args) {
+		 // create double type variable
+	    double num = 10.99;
+	    System.out.println("The double value: " + num);
+
+	    // convert into int type
+	    int data = (int)num;
+	    System.out.println("The integer value: " + data);
+
+	}
+
+}
